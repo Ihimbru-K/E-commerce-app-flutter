c1bd2c7b05bf44d63d26ca7ecd51896161730a91
Ihimbru-k <kanihims25@gmail.com>
2023-08-24 17:03:14 +0100

    created color class

---
+++ smartshop/lib/constants/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class  AppColor{
+  static const Color bgColor = Color(0xFFEEEFF5);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color purple = Color(0xFF693DA1);
+  static const Color red = Color(0xFFDA4040);
+  static const Color lblue = Color(0xFFADD8E6);
+  static const Color blue100 = Color(0xFF64B5F6);
+  static const Color fblue = Color(0xFF2074F0);
+  static const Color nblue = Color( 0xFF4682B4);
+  static const Color dblue = Color(0xFF0F1C3F);
+  static const Color Blue = Color(0xFF5F52EE);
+  static const Color green = Color(0xFF60D889);
+  static const Color dgreen = Color(0xFF006400);
+  static const Color black = Color(0xFF3A3A3A);
+  static const Color dgrey = Color(0xFF767970);
+  static const Color yellowish = Color(0xFFDEDC52);
+  static const Color reddish = Color(0xFFCE3A54);
+  static const Color grey = Color(0xFFDCDCDC);
+ static const Color mediumGrey = Color(0xFF808080);
+  static const Color bgcolor = Color(0xFFEEEFF5);
+}
