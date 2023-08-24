a8139b66bed65e27d0d2882a59196d023d0a32dc
Ihimbru-k <kanihims25@gmail.com>
2023-08-25 00:35:12 +0100

    finished creating custom text widgets

--- smartshop/lib/widgets/title_text.dart
+++ smartshop/lib/widgets/title_text.dart
@@ -7,7 +7,7 @@ class SubtitleTextWidget extends StatelessWidget {
     this.fontSize = 18,
     this.color,
     this.textDecoration = TextDecoration.none,
-   this.fontWeight = FontWeight.normal, this.fontStyle = FontStyle.normal,
+    this.fontWeight = FontWeight.normal, this.fontStyle = FontStyle.normal, this.maxLines = 1,
   }) : super(key: key);
 
   final String label;
@@ -16,12 +16,12 @@ class SubtitleTextWidget extends StatelessWidget {
   final FontWeight fontWeight;
   final Color? color;
   final TextDecoration textDecoration;
-
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
     return Text(
-      label, style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, fontStyle: fontStyle, color: color,
-         decoration: textDecoration),
+      label, maxLines: maxLines, style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, fontStyle: fontStyle, color: color,
+        decoration: textDecoration, overflow: TextOverflow.ellipsis),
     );
   }
 }
